d658f93ce5d011ecc05a2eae403286538c01ae8a
carlosmpr <carlosmpr0508@gmail.com>
2021-10-13 10:57:02 -0400

    api completed

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@
 from django.urls import include, path
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet, RatingViewSet
+from .views import MovieViewSet, RatingViewSet,UserViewSet
 router = DefaultRouter()
 router.register(r'movies',MovieViewSet)
 router.register(r'rating',RatingViewSet)
+router.register(r'users',UserViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -2,15 +2,21 @@ from rest_framework import viewsets , response , status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Movie,Rating
 from django.contrib.auth.models import User
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer,UserSerializer
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     authentication_clases = (TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=['post'])
     def rate_movie(self, request, pk=None):
@@ -35,9 +41,20 @@ class MovieViewSet(viewsets.ModelViewSet):
         else:
             response = {'message':'Rate the movie'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
+ 
 
 
-class RatingViewSet(viewsets.ModelViewSet):
+
+class RatingViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     authentication_clases = (TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def update(self,request,*args,**kwargs):
+        response = {'Operation not available'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+    def create(self,request,*args,**kwargs):
+        response = {'Operation not available'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
